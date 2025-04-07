afd62f8d840f995df0eaf8764e80b2091f084655
Leticia Mussolin <50248825+mussolin123@users.noreply.github.com>
2025-04-07 20:14:52 -0300

    Update ServerSocket.py

--- ServerSocket.py
+++ ServerSocket.py
@@ -1,27 +1,47 @@
 import socket
-def server(host = 'localhost', port=42000):
-    data_payload = 2048 #The maximum amount of data to be received at once
-    # Create a TCP socket
-    sock = socket.socket(socket.AF_INET,  socket.SOCK_STREAM)
-    # Enable reuse address/port 
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    # Bind the socket to the port
-    server_address = (host, port)
-    print ("Starting up echo server  on %s port %s" % server_address)
-    sock.bind(server_address)
-    # Listen to clients, argument specifies the max no. of queued connections
-    sock.listen(5) 
-    i = 0
-    while True: 
-        print ("Waiting to receive message from client")
-        client, address = sock.accept() 
-        data = client.recv(data_payload) 
-        if data:
-            print ("Data: %s" %data)
-            client.send(data)
-            print ("sent %s bytes back to %s" % (data, address))
-            # end connection
-            client.close()
-            i+=1
-            if i>=3: break           
-server()
+
+
+def run_server():
+    # create a socket object
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    server_ip = "127.0.0.1"
+    port = 8000
+
+    # bind the socket to a specific address and port
+    server.bind((server_ip, port))
+    # listen for incoming connections
+    server.listen(0)
+    print(f"Listening on {server_ip}:{port}")
+
+    # accept incoming connections
+    client_socket, client_address = server.accept()
+    print(f"Accepted connection from {client_address[0]}:{client_address[1]}")
+
+    # receive data from the client
+    while True:
+        request = client_socket.recv(1024)
+        request = request.decode("utf-8") # convert bytes to string
+        
+        # if we receive "close" from the client, then we break
+        # out of the loop and close the conneciton
+        if request.lower() == "close":
+            # send response to the client which acknowledges that the
+            # connection should be closed and break out of the loop
+            client_socket.send("closed".encode("utf-8"))
+            break
+
+        print(f"Received: {request}")
+
+        response = "accepted".encode("utf-8") # convert string to bytes
+        # convert and send accept response to the client
+        client_socket.send(response)
+
+    # close connection socket with the client
+    client_socket.close()
+    print("Connection to client closed")
+    # close server socket
+    server.close()
+
+
+run_server()
