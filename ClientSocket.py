4ae3b91c32a2d4a9332bfb86600d8c908af85984
Leticia Mussolin <50248825+mussolin123@users.noreply.github.com>
2025-04-07 20:14:38 -0300

    Update ClientSocket.py

--- ClientSocket.py
+++ ClientSocket.py
@@ -1,30 +1,32 @@
 import socket
-def client(host = 'localhost', port=42000): 
-    # Create a TCP/IP socket 
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-    # Connect the socket to the server 
-    server_address = (host, port) 
-    print ("Connecting to %s port %s" % server_address) 
-    sock.connect(server_address) 
-    # Send data 
-    try: 
-        # Send data 
-        message = "Test message. This will be echoed" 
-        print ("Sending %s" % message) 
-        sock.sendall(message.encode('utf-8')) 
-        # Look for the response 
-        amount_received = 0 
-        amount_expected = len(message) 
-        while amount_received < amount_expected: 
-            data = sock.recv(16) 
-            amount_received += len(data) 
-            print ("Received: %s" % data) 
-    except socket.error as e: 
-        print ("Socket error: %s" %str(e)) 
-    except Exception as e: 
-        print ("Other exception: %s" %str(e)) 
-    finally: 
-        print ("Closing connection to the server") 
-        sock.close() 
 
-client()
+
+def run_client():
+    # create a socket object
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    server_ip = "127.0.0.1"  # replace with the server's IP address
+    server_port = 8000  # replace with the server's port number
+    # establish connection with server
+    client.connect((server_ip, server_port))
+
+    while True:
+        # input message and send it to the server
+        msg = input("Enter message: ")
+        client.send(msg.encode("utf-8")[:1024])
+
+        # receive message from the server
+        response = client.recv(1024)
+        response = response.decode("utf-8")
+
+        # if server sent us "closed" in the payload, we break out of the loop and close our socket
+        if response.lower() == "closed":
+            break
+
+        print(f"Received: {response}")
+
+    # close client socket (connection to the server)
+    client.close()
+    print("Connection to server closed")
+
+run_client()
