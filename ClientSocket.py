f2cc9a8a27d98aa212cc6acd75b244f0722abc27
Leticia Mussolin <50248825+mussolin123@users.noreply.github.com>
2025-04-14 20:33:45 -0300

    Update ClientSocket.py

--- ClientSocket.py
+++ ClientSocket.py
@@ -17,7 +17,7 @@ def receber_mensagens(client_socket):
 def run_client():
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_ip = "127.0.0.1"
-    server_port = 8000
+    server_port = 42000
     client.connect((server_ip, server_port))
 
     # Recebe e define o nome de usuário
